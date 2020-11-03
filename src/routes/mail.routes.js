2fa0db8d2cef1b269cccf1a26918cabd518da008
gwalchmay <l.luck@laposte.net>
2020-11-03 17:24:05 +0100

    route done to get email address in contact form

--- src/routes/mail.routes.js
+++ src/routes/mail.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const nodemailer = require('nodemailer');
 const cors = require('cors');
+const passport = require('../../helpers/passport');
+const { connection } = require('../../connection');
 require('dotenv').config();
 
 const transporter = nodemailer.createTransport({
@@ -21,28 +23,41 @@ transporter.verify(function (error, success) {
     }
 });
 
+
 router.post('/', (req, res, next) => {
     const email = req.body.email
     const subject = req.body.subject
     const message = req.body.message
-    const content = `email: ${email} \n subject: ${subject} \n message: ${message} `
+    const content = `email: ${email} \n sujet: ${subject} \n message: ${message} `
     const mail = {
-      from: email,
-      to: process.env.EMAIL,
-      subject: "Une demande utilisateur a été effectuée",
-      text: content
-    } 
+        from: email,
+        to: process.env.EMAIL,
+        subject: "Une demande utilisateur a été effectuée",
+        text: content
+    }
     transporter.sendMail(mail, (err, data) => {
-      if (err) {
-        res.json({
-          status: 'fail'
-        })
-      } else {
-        res.json({
-         status: 'success'
-        })
-      }
+        if (err) {
+            res.json({
+                status: 'fail'
+            })
+        } else {
+            res.json({
+                status: 'success'
+            })
+        }
     })
-  })
+})
 
-  module.exports = router;
+router.get('/email', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const user = req.user.id;
+    connection.query(`SELECT email FROM users WHERE id = ${user}`, (err, results) => {
+        if (err) {
+            res.status(500).send(err.message);
+        } else {
+            res.json(results[0].email);
+        }
+    });
+});
+
+
+module.exports = router;
